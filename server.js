c12eb3db0870b03e75378656ccfb33e32ee7eb65
dddmadison <119731457+dddmadison@users.noreply.github.com>
2025-06-01 00:30:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const axios = require('axios');
+const cors = require('cors');
+const axios   = require('axios');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 // Use environment variables for database URL
 const dbUri = process.env.DB_URI;
